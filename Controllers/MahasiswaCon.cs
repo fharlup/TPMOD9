59f388cb1266956881b455b50e484769e3191d6e
fharlup <35958282+fharlup@users.noreply.github.com>
2024-04-28 18:52:59 +0700

    implentasi

---
+++ Controllers/MahasiswaCon.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class MahasiswaController
+{
+
+
+    static public List<Mahasiswa> listMhs = new List<Mahasiswa> {
+            new Mahasiswa("lebron", "13022220000"),
+            new Mahasiswa("curr", "130222001"),
+            };
+
+    [HttpGet]
+    public IEnumerable<Mahasiswa> Get()
+    {
+        return listMhs;
+    }
+
+    [HttpGet("{id}")]
+    public Mahasiswa Get(int id)
+    {
+        return listMhs[id];
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Mahasiswa value)
+    {
+        listMhs.Add(value);
+    }
+
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        listMhs.RemoveAt(id);
+    }
+}
---
+++ Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Mahasiswa
+{
+    public string Nama {  get; set; }
+    public string Nim {  get; set; }
+    public Mahasiswa()
+    {
+    }
+    public Mahasiswa(string Nama,string Nim)
+    {
+        this.Nama = Nama; this.Nim = Nim;
+    }
+}
